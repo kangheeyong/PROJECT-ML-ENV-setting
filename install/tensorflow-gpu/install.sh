86e03b226004857855f8d8538432dcd00a4e43c4
kang hee yong <cagojeiger@naver.com>
2020-06-18 19:48:59 +0900

    init all

--- install/tensorflow-gpu/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd -P )"
-
-sudo docker build -t local/ubuntu:18.04-tensorflow-gpu . -f $DIR"/Dockerfile"
-sudo docker run --gpus all -d -ti -p 8080-9000:8080-9000 --name jeiger local/ubuntu:18.04-tensorflow-gpu
-
-
-
-
