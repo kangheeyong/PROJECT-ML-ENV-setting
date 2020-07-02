d99e978b666da836cd1c5877127a92f72d104201
kang hee yong <cagojeiger@naver.com>
2020-07-02 21:45:59 +0900

    edit docker setting

--- install/tensorflow-gpu/install.sh
+++ install/tensorflow-gpu/install.sh
@@ -2,7 +2,10 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 
 sudo docker build -t local/ubuntu:18.04-tensorflow-gpu . -f $DIR"/Dockerfile"
-sudo docker run --gpus all -d -ti -p 8080-9000:8080-9000 --name jeiger local/ubuntu:18.04-tensorflow-gpu
+sudo docker run -d -ti -p 18080-19000:18080-19000 --name jeiger\
+                -e NVIDIA_VISIBLE_DEVICES=1,2 \
+                -v ~/jeiger:/root/jeiger\
+                local/ubuntu:18.04-tensorflow-gpu
 
 
 
